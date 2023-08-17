2732f55c0088d0e3777fe8c44db01322d9e7bda6
HoAnhKhoa <traincolab2131999v1@gmail.com>
2023-08-17 20:02:41 +0700

    Change root path

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 sh run.py --batch_size 8192 \
           --save_model_path '/content/drive/MyDrive/Master/NLP/Baseline/Protonet/5w1s' \
-          --root /content/dataset_han_nom \
+          --root /content/dataset_han_nom_real_synth \
           --epochs 100 \
           --startIter 0 \
           --maxIter 100000 \
