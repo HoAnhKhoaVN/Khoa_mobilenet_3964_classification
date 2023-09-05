12296da7a75a783cc2bcde81cebdeea1e34961a7
HoAnhKhoa <traincolab2131999v1@gmail.com>
2023-09-05 14:11:46 +0700

    Add code to train on colab

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 python run.py --batch_size 64 \
-          --save_model_path '/content/drive/MyDrive/Master/NomTemple/CLS' \
+          --save_model_path '/content/drive/MyDrive/Master/NomTemple/CLS/v1' \
           --dataset_root  /content/han_nom_cls_v2 \
           --epochs 100 \
           --startIter 0 \
           --maxIter 1000 \
           --valIter 30 \
-          --train \
-          --test \
+          --train True \
+          --test True \
           --learning_rate 1.0 \
           --rho 0.9 \
           --eps 1e-05 \
