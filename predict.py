1f4ec6580030c286db0dab87ac02f3736db80212
HoAnhKhoa <traincolab2131999v1@gmail.com>
2023-09-05 21:54:29 +0700

    Add code print result

--- predict.py
+++ predict.py
@@ -5,11 +5,47 @@ import torch
 from torch import nn
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 import numpy as np
-from train import evaluate
-import torch.optim as optim
 from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from prepare_ds import transforms
+from PIL import Image
+import torch.nn.functional as F
+
+
+IMG_SIZE = 224
+transform = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Resize((IMG_SIZE, IMG_SIZE), antialias= False),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ])
+class RawDataset(Dataset):
+
+    def __init__(self, root):
+        self.image_path_list = []
+        for dirpath, dirnames, filenames in os.walk(root):
+            for name in filenames:
+                _, ext = os.path.splitext(name)
+                ext = ext.lower()
+                if ext == '.jpg' or ext == '.jpeg' or ext == '.png':
+                    self.image_path_list.append(os.path.join(dirpath, name))
+
+        # self.image_path_list = natsorted(self.image_path_list)
+        self.nSamples = len(self.image_path_list)
+
+    def __len__(self):
+        return self.nSamples
+
+    def __getitem__(self, index):
+        try:
+            img = Image.open(self.image_path_list[index]).convert('RGB')  # for color image
+            img = transform(img)
+        except IOError:
+            print(f'Corrupted image for {index}')
+            # make dummy image and dummy label for corrupted image.
+            img = Image.new('RGB', (self.opt.imgW, self.opt.imgH))
+
+        return (img, self.image_path_list[index])
 
 
 def predict():
@@ -62,8 +98,8 @@ def predict():
     print('Trainable params num : ', sum(params_num))
     # endregion
 
-    # region get loader
-    predictset = ImageFolder(root=args.dataset_root, transform=transforms)
+    # region prepare data
+    predictset = RawDataset(root = args.dataset_root)
     predictloader = DataLoader(predictset,
                                batch_size=args.batch_size,
                                shuffle=True,
@@ -72,19 +108,22 @@ def predict():
     # endregion
 
     # region test
-    model.eval()
-    test_acc, _ = evaluate(
-        model,
-        predictloader,
-        criterion,
-        device
-    )
-        
-    with open(f'{args.save_model_path}/test.log', 'w', encoding='utf-8') as log:
-        log.write(f'Accuracy: {test_acc}')
-    print(f'Accuracy: {test_acc}')
+    
+    with open(f'{args.save_model_path}/log_predict_result.txt', 'w') as log:
+        model.eval()
+        with torch.no_grad():
+            for image_tensors, image_path_list in predictloader:
+                images = image_tensors.to(device)
+                print(f'Size images: {images.size()}')
+                preds = model(images)
+                print(f'Size predictions: {preds.size()}')
+                preds_prob, preds = preds.max(1)
 
-
+                print(f'Predictions: {preds}')
+                print(f'Size predictions probability: {preds_prob}')
+                for img_name, pred, pred_max_prob in zip(image_path_list, preds, preds_prob):
+                    str_val = str(pred.item())
+                    log.write(f'{img_name:25s}\t{str_val:25s}\t{pred_max_prob}\n')
     # endregion
 
 if __name__ == '__main__':
--- predict.sh
+++ predict.sh
@@ -1,8 +1,8 @@
 echo Predict
 python predict.py --dataset_root /content/han_nom_kinh_ky \
-                  --save_model_path /content/drive/MyDrive/Master/NomTemple/CLS/v1 \
-                  --checkpoint_path /content/drive/MyDrive/Master/NomTemple/CLS/v1/best_accuracy.pth \
+                  --save_model_path /content/drive/MyDrive/Master/NomTemple/CLS/v2 \
+                  --checkpoint_path /content/drive/MyDrive/Master/NomTemple/CLS/v2/best_accuracy.pth \
                   --mode han_nom_cls \
-                  --batch_size 1024
+                  --batch_size 64
 
 echo zip file
